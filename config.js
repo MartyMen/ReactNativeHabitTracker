2cc7a8304f90ee6a8f556bff5b7265c7a5f55f03
MartyMen <60930141+MartyMen@users.noreply.github.com>
2024-05-23 08:52:58 +0100

    Update config.js
    removed api key and url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const Config = {
-    PROJECT_URL: 'https://vihmumajevtirrjtjxkj.supabase.co',
-    API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaG11bWFqZXZ0aXJyanRqeGtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2NDAxODIsImV4cCI6MjAyNTIxNjE4Mn0.DzWYzFNtVOSl9Yzh48XjGv4p4hyWNM9vup4uZisfLZY',
+    PROJECT_URL: 'URL HERE SUPABASE',
+    API_KEY: 'API KEY HERE',
 };
 export default Config;
